a5c9e963f5830502803f2003ca221fc7336459a7
Dmitry Timchenko <112801926+Alisha192@users.noreply.github.com>
2024-10-29 12:43:02 +0300

    Create Proof.py

---
+++ Proof.py
@@ -0,0 +1,11 @@
+from expression import Expression
+from typing import List, Optional
+
+class Proof:
+    def prove(self, axioms: List[Expression], target: Expression) -> bool:
+        # TODO: реализовать
+        return False
+
+    def apply_modus_ponens(self, expressions: List[Expression]) -> Optional[Expression]:
+        # TODO: реализовать
+        return None
