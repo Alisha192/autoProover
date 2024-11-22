19625e839fd18f6d4de4223509792de835f6d797
Dmitry Timchenko <112801926+Alisha192@users.noreply.github.com>
2024-11-22 14:51:28 +0300

    Create language.py

---
+++ language.py
@@ -0,0 +1,366 @@
+from functools import reduce
+class Variable:
+  def __init__(self, name):
+    self.name = name
+    self.time = 0
+
+  def freeVariables(self):
+    return { self }
+
+  def freeUnificationTerms(self):
+    return set()
+
+  def replace(self, old, new):
+    if self == old:
+      return new
+    return self
+
+  def occurs(self, unification_term):
+    return False
+
+  def setInstantiationTime(self, time):
+    self.time = time
+
+  def to_implication(self):
+    return self
+
+  def __eq__(self, other):
+    if not isinstance(other, Variable):
+      return False
+    return self.name == other.name
+
+  def __str__(self):
+    return self.name
+
+  def __hash__(self):
+    return hash(str(self))
+
+class UnificationTerm:
+  def __init__(self, name):
+    self.name = name
+    self.time = 0
+
+  def freeVariables(self):
+    return set()
+
+  def freeUnificationTerms(self):
+    return { self }
+
+  def replace(self, old, new):
+    if self == old:
+      return new
+    return self
+
+  def occurs(self, unification_term):
+    return self == unification_term
+
+  def setInstantiationTime(self, time):
+    self.time = time
+
+  def __eq__(self, other):
+    if not isinstance(other, UnificationTerm):
+      return False
+    return self.name == other.name
+
+  def __str__(self):
+    return self.name
+
+  def __hash__(self):
+    return hash(str(self))
+
+class Function:
+  def __init__(self, name, terms):
+    self.name = name
+    self.terms = terms
+    self.time = 0
+
+  def freeVariables(self):
+    if len(self.terms) == 0:
+      return set()
+    return reduce((lambda x, y: x | y),
+      [term.freeVariables() for term in self.terms])
+
+  def freeUnificationTerms(self):
+    if len(self.terms) == 0:
+      return set()
+    return reduce((lambda x, y: x | y),
+      [term.freeUnificationTerms() for term in self.terms])
+
+  def replace(self, old, new):
+    if self == old:
+      return new
+    return Function(self.name,
+      [term.replace(old, new) for term in self.terms]
+    )
+
+  def occurs(self, unification_term):
+    return any([term.occurs(unification_term) for term in self.terms])
+
+  def setInstantiationTime(self, time):
+    self.time = time
+    for term in self.terms:
+      term.setInstantiationTime(time)
+
+  def __eq__(self, other):
+    if not isinstance(other, Function):
+      return False
+    if self.name != other.name:
+      return False
+    if len(self.terms) != len(other.terms):
+      return False
+    return all(
+      [self.terms[i] == other.terms[i] for i in range(len(self.terms))]
+    )
+
+  def __str__(self):
+    if len(self.terms) == 0:
+      return self.name
+    return self.name + '(' + ', '.join(
+      [str(term) for term in self.terms]
+    ) + ')'
+
+  def __hash__(self):
+    return hash(str(self))
+
+##############################################################################
+# Formulae
+##############################################################################
+
+class Predicate:
+  def __init__(self, name, terms):
+    self.name = name
+    self.terms = terms
+
+  def freeVariables(self):
+    if len(self.terms) == 0:
+      return set()
+    return reduce((lambda x, y: x | y),
+      [term.freeVariables() for term in self.terms])
+
+  def freeUnificationTerms(self):
+    if len(self.terms) == 0:
+      return set()
+    return reduce((lambda x, y: x | y),
+      [term.freeUnificationTerms() for term in self.terms])
+
+  def replace(self, old, new):
+    if self == old:
+      return new
+    return Predicate(self.name,
+      [term.replace(old, new) for term in self.terms]
+    )
+
+  def occurs(self, unification_term):
+    return any([term.occurs(unification_term) for term in self.terms])
+
+  def __eq__(self, other):
+    if not isinstance(other, Predicate):
+      return False
+    if self.name != other.name:
+      return False
+    if len(self.terms) != len(other.terms):
+      return False
+    return all(
+      [self.terms[i] == other.terms[i] for i in range(len(self.terms))]
+    )
+
+  def setInstantiationTime(self, time):
+    for term in self.terms:
+      term.setInstantiationTime(time)
+
+  def __str__(self):
+    if len(self.terms) == 0:
+      return self.name
+    return self.name + '(' + ', '.join(
+      [str(term) for term in self.terms]
+    ) + ')'
+
+  def __hash__(self):
+    return hash(str(self))
+
+  def to_implication(self):
+    return self
+
+class Not:
+  def __init__(self, formula):
+    self.formula = formula
+
+  def freeVariables(self):
+    return self.formula.freeVariables()
+
+  def freeUnificationTerms(self):
+    return self.formula.freeUnificationTerms()
+
+  def replace(self, old, new):
+    if self == old:
+      return new
+    return Not(self.formula.replace(old, new))
+
+  def occurs(self, unification_term):
+    return self.formula.occurs(unification_term)
+
+  def setInstantiationTime(self, time):
+    self.formula.setInstantiationTime(time)
+
+  def __eq__(self, other):
+    if not isinstance(other, Not):
+      return False
+    return self.formula == other.formula
+
+  def __str__(self):
+    return '¬' + str(self.formula)
+
+  def __hash__(self):
+    return hash(str(self))
+
+  def to_implication(self):
+    return Not(self.formula.to_implication())
+
+class And:
+  def __init__(self, formula_a, formula_b):
+    self.formula_a = formula_a
+    self.formula_b = formula_b
+
+  def freeVariables(self):
+    return self.formula_a.freeVariables() | \
+      self.formula_b.freeVariables()
+
+  def freeUnificationTerms(self):
+    return self.formula_a.freeUnificationTerms() | \
+      self.formula_b.freeUnificationTerms()
+
+  def replace(self, old, new):
+    if self == old:
+      return new
+    return And(
+      self.formula_a.replace(old, new),
+      self.formula_b.replace(old, new)
+    )
+
+  def occurs(self, unification_term):
+    return self.formula_a.occurs(unification_term) or \
+      self.formula_b.occurs(unification_term)
+
+  def setInstantiationTime(self, time):
+    self.formula_a.setInstantiationTime(time)
+    self.formula_b.setInstantiationTime(time)
+
+  def __eq__(self, other):
+    if not isinstance(other, And):
+      return False
+    return self.formula_a == other.formula_a and \
+      self.formula_b == other.formula_b
+
+  def __str__(self):
+    return '(%s ∧ %s)' % (self.formula_a, self.formula_b)
+
+  def __hash__(self):
+    return hash(str(self))
+
+  def to_implication(self):
+    return Not((Implies(self.formula_a.to_implication(), Not(self.formula_b.to_implication()))))
+
+
+class Or:
+  def __init__(self, formula_a, formula_b):
+    self.formula_a = formula_a
+    self.formula_b = formula_b
+
+  def freeVariables(self):
+    return self.formula_a.freeVariables() | \
+      self.formula_b.freeVariables()
+
+  def freeUnificationTerms(self):
+    return self.formula_a.freeUnificationTerms() | \
+      self.formula_b.freeUnificationTerms()
+
+  def replace(self, old, new):
+    if self == old:
+      return new
+    return Or(
+      self.formula_a.replace(old, new),
+      self.formula_b.replace(old, new)
+    )
+
+  def occurs(self, unification_term):
+    return self.formula_a.occurs(unification_term) or \
+      self.formula_b.occurs(unification_term)
+
+  def setInstantiationTime(self, time):
+    self.formula_a.setInstantiationTime(time)
+    self.formula_b.setInstantiationTime(time)
+
+  def __eq__(self, other):
+    if not isinstance(other, Or):
+      return False
+    return self.formula_a == other.formula_a and \
+      self.formula_b == other.formula_b
+
+  def __str__(self):
+    return '(%s ∨ %s)' % (self.formula_a, self.formula_b)
+
+  def __hash__(self):
+    return hash(str(self))
+
+  def to_implication(self):
+    return Implies(Not(self.formula_a), self.formula_b)
+
+class Implies:
+  def __init__(self, formula_a, formula_b):
+    self.formula_a = formula_a
+    self.formula_b = formula_b
+
+  def freeVariables(self):
+    return self.formula_a.freeVariables() | \
+      self.formula_b.freeVariables()
+
+  def freeUnificationTerms(self):
+    return self.formula_a.freeUnificationTerms() | \
+      self.formula_b.freeUnificationTerms()
+
+  def replace(self, old, new):
+    if self == old:
+      return new
+    return Implies(
+      self.formula_a.replace(old, new),
+      self.formula_b.replace(old, new)
+    )
+
+  def occurs(self, unification_term):
+    return self.formula_a.occurs(unification_term) or \
+      self.formula_b.occurs(unification_term)
+
+  def setInstantiationTime(self, time):
+    self.formula_a.setInstantiationTime(time)
+    self.formula_b.setInstantiationTime(time)
+
+  def __eq__(self, other):
+    if not isinstance(other, Implies):
+      return False
+    return self.formula_a == other.formula_a and \
+      self.formula_b == other.formula_b
+
+  def __str__(self):
+    return '(%s → %s)' % (self.formula_a, self.formula_b)
+
+  def __hash__(self):
+    return hash(str(self))
+
+  def to_implication(self):
+    return Implies(Not(self.formula_a).to_implication(), self.formula_b.to_implication())
+
+class XOR:
+  def __init__(self, formula_a, formula_b):
+    self.formula_a = formula_a
+    self.formula_b = formula_b
+
+  def to_implication(self):
+    return And(Or(Not(self.formula_a), Not(self.formula_b)).to_implication(),Or(self.formula_a, self.formula_b).to_implication()).to_implication()
+
+class Equivalence:
+  def __init__(self, formula_a, formula_b):
+    self.formula_a = formula_a
+    self.formula_b = formula_b
+
+  def to_implication(self):
+    return And(Or(Not(self.formula_a).to_implication(), self.formula_b.to_implication()).to_implication(), Or(Not(self.formula_b).to_implication(), self.formula_a.to_implication()).to_implication()).to_implication()
